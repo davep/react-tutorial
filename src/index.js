85156c1644afb755710fff663d72709b3dac2dd4
Dave Pearson <davep@davep.org>
2017-11-17 11:35:48 +0000

    Different cursor depending on if a square can be clicked

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function Square( props ) {
     return (
-        <button className="square" onClick={ () => props.onClick() }>
+        <button className={ "square " + ( props.value === null ? "empty" : "filled")} onClick={ () => props.onClick() }>
           {props.value}
         </button>
     );
